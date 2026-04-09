1c34b6f182689e95bdc9c19e58f45785e69f2d0c
Filip Živsa <filip.zivsa@gmail.com>
2026-04-09 20:16:16 +0200

    added Console helper file

---
+++ zalohovac-editor/Helpers/ConsoleHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace GradeRegisterAdminApp.Helpers
+{
+    public static class ConsoleHelper
+    {
+        public static void WriteConditionalColor(object? value, bool condition, ConsoleColor color)
+        {
+            if (condition)
+                Console.ForegroundColor = color;
+            Console.Write(value);
+            Console.ResetColor();
+        }
+        public static void WriteLineConditionalColor(object? value, bool condition, ConsoleColor color)
+        {
+            WriteConditionalColor(value, condition, color);
+            Console.WriteLine();
+        }
+    }
+}
