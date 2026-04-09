6284ee6ac3d9a3cc7200f11a5bcb160b2a21dae9
Filip Živsa <filip.zivsa@gmail.com>
2026-04-09 20:15:32 +0200

    imported application.cs

---
+++ zalohovac-editor/Application.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zalohovac_editor
+{
+    public class Application
+    {
+        private bool _running;
+        private IWindow? _activeWindow;
+
+        public Application()
+        {
+            _running = false;
+            _activeWindow = null;
+        }
+
+        public void Run(IWindow window)
+        {
+            _running = true;
+            _activeWindow = window;
+
+            Console.Title = "GradeRegister Admin [v1.0]";
+            Console.CursorVisible = false;
+            Console.Clear();
+
+            while (_running)
+            {
+                Render();
+                HandleKey(Console.ReadKey(true));
+            }
+        }
+        public void Stop()
+        {
+            _running = false;
+        }
+        public void SwitchWindow(IWindow window)
+        {
+            Console.Clear();
+            _activeWindow = window;
+        }
+
+        protected void Render()
+        {
+            Console.SetCursorPosition(0, 0);
+            _activeWindow?.Render();
+        }
+        protected void HandleKey(ConsoleKeyInfo keyInfo)
+        {
+            _activeWindow?.HandleKey(keyInfo);
+        }
+    }
+}
